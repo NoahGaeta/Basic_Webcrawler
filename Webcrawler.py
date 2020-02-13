1bc1c37871b6e2f59d639083eafa57dbccdfe870
Noah Gaeta <ngaet02@gmail.com>
2020-02-13 12:13:17 -0500

    Initial Commit

--- Webcrawler.py
+++ Webcrawler.py
@@ -2,49 +2,64 @@
 from __future__ import print_function
 
 from urllib import urlopen
-from time import time
+from multiprocessing import Process, Manager
+import multiprocessing as mp
 import re
 import json
 
 
 class Webcrawler:
 
-	def __init__(self, start_url, crawl_time, already_crawled=[]):
+	URL_JSON_FILE_PATH = 'urls.json'
+
+	def __init__(self, start_url, crawl_amount, already_crawled=[]):
 		self.start_url = start_url
-		self.crawl_time = crawl_time
+		self.crawl_amount = crawl_amount
 		self.already_crawled = already_crawled
 
-	def find_urls(self, response):
+	def find_url_pattern_matches(self, response):
 		urls_found = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', response)
 		return self.remove_duplicates(urls_found)
 
 	def start_crawl(self):
-		start_url = self.start_url
-		response = self.get_response(start_url)
-		url_list = self.find_urls(response)
-		self.already_crawled = [start_url]
+		response = self.get_response(self.start_url)
+		url_list = self.find_url_pattern_matches(response)
+
+		self.already_crawled = list(self.start_url)
 		returned_list = self.crawl(url_list)
-		start_time = time()
-		while self.get_time_left(start_time) < self.crawl_time:
+		while len(returned_list) < self.crawl_amount:
 			returned_list = self.crawl(returned_list)
+		self.dump_urls(returned_list, self.URL_JSON_FILE_PATH)
 
 	def crawl(self, url_list):
-		for url in url_list:
-			if url not in self.already_crawled:
-				try:
-					response = self.get_response(url)
-					print("finding urls on " + url)
-					url_list += (self.find_urls(response))
-					url_list = self.remove_duplicates(url_list)
-					self.already_crawled.append(url)
-					with open('urls.json', 'w') as outfile:
-						json.dump(url_list, outfile, indent=4)
-				except Exception as e:
-					self.already_crawled.append(url)
-					print("Exception: " + str(e) + "\ncontinuing")
-			else:
-				print("Already crawled: " + url)
-		return url_list
+		processes = []
+		result_manager = Manager()
+		results = result_manager.list()
+		map(lambda url: processes.append(Process(target=self.crawl_url, args=(url, results,))), url_list)
+		chunked_processes = self.split_list(processes, mp.cpu_count() - 1)
+		for chunk in chunked_processes:
+			map(lambda process: process.start(), chunk)
+			map(lambda process: process.join(), chunk)
+		return list(results)
+
+	def crawl_url(self, url, results):
+		if url not in self.already_crawled:
+			results += self.find_urls(url)
+
+	def find_urls(self, url):
+		try:
+			response = self.get_response(url)
+			self.already_crawled.append(url)
+			return self.find_url_pattern_matches(response)
+		except Exception as e:
+			self.already_crawled.append(url)
+			print("Exception: " + str(e) + "\ncontinuing")
+			return []
+
+	@staticmethod
+	def dump_urls(url_list, path):
+		with open(path, 'w') as outfile:
+			json.dump(url_list, outfile, indent=4)
 
 	@staticmethod
 	def get_response(url):
@@ -58,6 +73,6 @@ class Webcrawler:
 		return list(set(url_list))
 
 	@staticmethod
-	def get_time_left(start_time):
-		print(str(time() - start_time))
-		return time() - start_time
+	def split_list(l, n):
+		for i in range(0, len(l), n):
+			yield l[i:i + n]
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Author: Noah Gaeta -- Date: 02/28/2019wq
+# Author: Noah Gaeta -- Date: 02/28/2019
 from Webcrawler import Webcrawler
 import argparse
 import os
@@ -7,12 +7,12 @@ import os
 def main():
 	parser = argparse.ArgumentParser(description='Webcrawler that obtains urls.')
 	parser.add_argument('--url', default="https://www.derivative-calculator.net/", help='the start url')
-	parser.add_argument('--time', default=60, help='time to crawl in seconds')
+	parser.add_argument('--amount', default=1000, help='')
 	parser.add_argument('--clean', default=False, action='store_true', help='removes urls.json file')
 	args = parser.parse_args()
-	if args.clean:
+	if args.clean and os.path.exists("urls.json"):
 		os.remove("urls.json")
-	webcrawler = Webcrawler(args.url, args.time)
+	webcrawler = Webcrawler(args.url, args.amount)
 	webcrawler.start_crawl()
 
 
